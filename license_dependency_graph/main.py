be0ef86d920e68962082c1c4bc6c47f9eb80016a
maximlopin <wzzhz13@gmail.com>
2020-06-26 15:32:35 +0300

    visualize tree

--- license_dependency_graph/main.py
+++ license_dependency_graph/main.py
@@ -76,7 +76,7 @@ def _iter_pypi(package_name, cache=dict()):
             continue
 
 
-def _get_license_and_requirements(url, cache=dict()):
+def _get_licenses_and_requirements(url, cache=dict()):
     dist_name = url.split('/')[-1]
     dist_path = os.path.join(PACKAGES_DIR, dist_name)
 
@@ -89,69 +89,64 @@ def _get_license_and_requirements(url, cache=dict()):
     requirements = set()
     licenses = set()
 
+    def read_pkg_info(data):
+        pkg_info = EMAIL_PARSER.parsestr(data)
+        for k, v in pkg_info.items():
+            if k.lower() == 'license':
+                licenses.add(v)
+
+    def read_metadata_json(data):
+        metadata = json.loads(data)
+        for k, v in metadata.items():
+            if k.lower() == 'license':
+                licenses.add(v)
+
+    def read_requirements(data):
+        lines = data.split('\n')
+        for line in map(lambda l: l.strip(), lines):
+            if len(line) > 0 and line[0].isalpha():
+                requirements.add(line)
+
     if dist_name.endswith('.tar.gz'):
         with tarfile.open(dist_path, 'r:gz') as tar:
             for member in tar.getmembers():
-                if member.name.endswith('PKG-INFO'):
+                name = member.name.lower()
+            
+                if 'pkg-info' in name:
                     pkg_info = tar.extractfile(member).read().decode()
-                    pkg_info_dict = EMAIL_PARSER.parsestr(pkg_info)
-                    if 'license' in pkg_info_dict:
-                        license_name = pkg_info_dict['license']
-                    elif 'License' in pkg_info_dict:
-                        license_name = pkg_info_dict['License']
-                    else:
-                        license_name = 'PKG-INFO doesn\'t contain license'
-                    break
-            else:
-                license_name = 'Failed to find PKG-INFO'
+                    read_pkg_info(pkg_info)
 
-            requirements = []
-            for member in tar.getmembers():
-                if member.name.endswith('requires.txt') or member.name.endswith('requirements.txt'):
-                    lines = tar.extractfile(member).read().decode().split('\n')
-                    requirements = []
-                    for l in lines:
-                        if len(l.strip()) > 0 and l.strip()[0].isalpha():
-                            requirements.append(l)
-                    break
-            else:
-                pass
-                #print('Warning: failed to find requires.txt/requirements.txt in {}'.format(dist_name))
+                elif 'requires' in name or 'requirements' in name:
+                    data = tar.extractfile(member).read().decode()
+                    read_requirements(data)
 
     elif dist_name.endswith('.zip') or dist_name.endswith('.whl'):
         with zipfile.ZipFile(dist_path) as zip_:
             for member in zip_.namelist():
-                if member.endswith('metadata.json'):
-                    metadata_str = zip_.read(member).decode()
-                    metadata_dict = json.loads(metadata_str)
-                    if 'license' in metadata_dict:
-                        license_name = metadata_dict['license']
-                    elif 'License' in metadata_dict:
-                        license_name = metadata_dict['License']
-                    else:
-                        license_name = 'metadata.json doesn\'t contain license'
-                    break
-                elif member.endswith('METADATA'):
-                    metadata = zip_.read(member).decode()
-                    metadata_dict = EMAIL_PARSER.parsestr(pkg_info)
-                    exit(zip_.read(member).decode())
-                finally:
+                name = member.split('/')[0].lower()
 
-            else:
-                license_name = 'Found no license info'
-                #print('Warning: failed to find metadata.json in {}'.format(dist_name))
-            
-            requirements = []
+                if 'metadata.json' in name:
+                    metadata_json = zip_.read(member).decode()
+                    read_metadata_json(metadata_json)
+
+                elif 'metadata' in name:
+                    metadata = zip_.read(member).decode()
+                    read_pkg_info(metadata)
+
+                elif 'requires' in name or 'requirements' in name:
+                    data = zip_.read(member).decode()
+                    read_requirements(data)
+
     else:
         raise Exception('exptected tar/zip/whl')
-    
-    return (license_name, requirements)
+
+    return list(licenses), list(requirements)
 
 
-def dep_graph(package_name, D, whitelist=None, branch_cache=dict(), visited_chain=list(),):
-    print('[{}]'.format(' -> '.join(visited_chain)))
+def dep_graph(package_name, D, whitelist=None, branch_cache=dict(), visited_chain=list()):
     name, *versions = _split_package_name(package_name)
 
+    # print('[{}]'.format(' -> '.join([*visited_chain, name])))
     try:
         # group by version
         releases = defaultdict(list)
@@ -195,15 +190,22 @@ def dep_graph(package_name, D, whitelist=None, branch_cache=dict(), visited_chai
 
         key = str((name, release_v))
 
+        print(' ' * (len(visited_chain) * 2), end='')
+        print('{}=={}'.format(name, release_v), end='')
+
         if whitelist is not None and name.lower() in whitelist:
-            D[key] = 'Whitelisted'
+            print(' [whitelisted]')
+            D[key] = 'whitelisted'
         elif name in visited_chain:
-            D[key] = 'CircularDependency'
+            print(' [circular dependency]')
+            D[key] = 'circular dependency'
         else:
             if key in branch_cache:
+                print(' [cached branch]')
                 D[key] = branch_cache[key]
             else:
-                license_name, requirements = _get_license_and_requirements(link)
+                print()
+                license_name, requirements = _get_licenses_and_requirements(link)
                 D[key] = {'license': license_name, 'requirements': dict()}
                 branch_cache[key] = D[key]
                 for req in requirements:
