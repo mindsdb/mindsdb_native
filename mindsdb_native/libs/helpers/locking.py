78cbbc51333c51fe61a436237b5687e60e560540
maximlopin <wzzhz13@gmail.com>
2020-07-02 13:21:05 +0300

    remove comments

--- mindsdb_native/libs/helpers/locking.py
+++ mindsdb_native/libs/helpers/locking.py
@@ -46,32 +46,3 @@ def mdb_lock(flags, lock_name, argname=None):
 
         return wrapper2
     return wrapper1
-
-
-# def learn_lock(name):
-#     learn_lock_fn = os.path.join(CONFIG.MINDSDB_STORAGE_PATH,f'{name}_learn.lock')
-#     f = open(learn_lock_fn, 'a+')
-#     portalocker.lock(f, portalocker.LOCK_EX+portalocker.LOCK_NB)
-#     return f
-
-# def predict_lock(name):
-#     learn_lock_fn = os.path.join(CONFIG.MINDSDB_STORAGE_PATH,f'{name}_learn.lock')
-#     f = open(learn_lock_fn, 'a+')
-#     portalocker.lock(f, portalocker.LOCK_SH+portalocker.LOCK_NB)
-#     return f
-
-# def delete_lock(name):
-#     learn_lock(name)
-#     delete_lock_fn = os.path.join(CONFIG.MINDSDB_STORAGE_PATH,f'{name}_delete.lock')
-#     f = open(delete_lock_fn, 'a+')
-#     portalocker.lock(f, portalocker.LOCK_EX+portalocker.LOCK_NB)
-#     return f
-
-# def get_data_lock(name):
-#     delete_lock_fn = os.path.join(CONFIG.MINDSDB_STORAGE_PATH,f'{name}_delete.lock')
-#     f = open(delete_lock_fn, 'a+')
-#     portalocker.lock(f, portalocker.LOCK_SH+portalocker.LOCK_NB)
-#     return f
-
-# def unlock(file):
-#     portalocker.unlock(file)
