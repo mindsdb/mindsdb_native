dee1dcf22a6b4150001fbdfc1a5ee66695d21eab
maximlopin <wzzhz13@gmail.com>
2020-07-07 10:26:10 +0300

    mongodb ds + ci test

--- mindsdb_native/libs/data_sources/mongodb_ds.py
+++ mindsdb_native/libs/data_sources/mongodb_ds.py
@@ -5,34 +5,21 @@ from mindsdb_native.libs.data_types.data_source import DataSource
 
 
 class MongoDS(DataSource):
+    def _setup(self, host='localhost', port=27017, user='admin', password='123',
+               database='database', collection='collection'):
 
-    def _setup(self, query=None, table, host='localhost', port=27017,
-               user='postgres', password='', database='postgres'):
+        conn = MongoClient(host=host,
+                           port=port,
+                           username=user,
+                           password=password)
 
-        if query is None:
-            query = f'SELECT * FROM {table}'
-
-        conn = MongoClient(host=host, post=port, username=user,
-                           password=password, authSource=database,
-                           authMechanism='SCRAM-SHA-256')
-        
         db = conn[database]
-        cursor = db[collection].find(query)
+        collection = db[collection]
 
-        df = pd.DataFrame(list(cursor))
-
-        df.columns = [x.decode('utf-8') for x in df.columns]
-        for col_name in df.columns:
-            try:
-                df[col_name] = df[col_name].apply(lambda x: x.decode("utf-8"))
-            except:
-                pass
+        df = pd.DataFrame(list(collection.find({}, {'_id': 0})))
 
         col_map = {}
         for col in df.columns:
             col_map[col] = col
 
         return df, col_map
-
-
-if __name__ == '__main__':
---
+++ tests/integration_tests/data_sources/test_mongodb_ds.py
@@ -0,0 +1,48 @@
+import pytest
+import logging
+from pymongo import MongoClient
+
+from mindsdb_native import Predictor
+from mindsdb_native.libs.data_sources.mongodb_ds import MongoDS
+from mindsdb_native import F
+
+@pytest.mark.integration
+def test_mongodb_ds():
+
+    HOST = 'localhost'
+    USER = 'admin'
+    PASSWORD = '123'
+    DATABASE = 'database'
+    COLLECTION_NAME = 'test_mindsdb'
+    PORT = 27017
+
+    con = MongoClient(host=HOST,
+                      port=PORT,
+                      username=USER,
+                      password=PASSWORD)
+
+    db = con[DATABASE]
+    
+    if COLLECTION_NAME in db.list_collection_names():
+        db[COLLECTION_NAME].drop()
+
+    collection = db[COLLECTION_NAME]
+
+    for i in range(0, 200):
+        collection.insert_one({
+            'col_1': "This is string number {}".format(i),
+            'col_2': i,
+            'col_3': (i % 2) == 0
+        })
+
+    mongodb_ds = MongoDS(host=HOST,
+                         port=PORT,
+                         user=USER,
+                         password=PASSWORD,
+                         database=DATABASE,
+                         collection=COLLECTION_NAME)
+
+    assert (len(mongodb_ds._df) == 200)
+
+    mdb = Predictor(name='analyse_dataset_test_predictor', log_level=logging.ERROR)
+    F.analyse_dataset(from_data=mongodb_ds)
