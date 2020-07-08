28028d6009978a04e34d78a7cc0169af5b0fdc89
maximlopin <wzzhz13@gmail.com>
2020-07-08 19:18:29 +0300

    use pytds

--- mindsdb_native/libs/data_sources/ms_sql_ds.py
+++ mindsdb_native/libs/data_sources/ms_sql_ds.py
@@ -1,33 +1,21 @@
 import pandas as pd
-import pyodbc
+import pytds
 from mindsdb_native.libs.data_types.data_source import DataSource
 
 
-def mssql_connect(host, port, user, password, database):
-    con_info = {
-        'DRIVER': '{ODBC Driver 17 for SQL Server}',
-        'SERVER': '{},{}'.format(host, port),
-        'DATABASE': str(database),
-        'UID': str(user),
-        'PWD': str(password)
-    }
-    con_info_str = ';'.join('{}={}'.format(k, v) for k, v in con_info.items())
-    return pyodbc.connect(con_info_str)
-
-
 class MSSQLDS(DataSource):
-    def _setup(self, query=None, host='.', user='sa', password='',
-               database='mssql', port=1433, table=None):
+    def _setup(self, query=None, host='localhost', user='sa', password='',
+               database='master', port=1433, table=None):
 
         if query is None:
             query = f'SELECT * FROM {table}'
 
-        con = mssql_connect(host=host, port=port, user=user,
-                            password=password, database=database)
-
-        df = pd.read_sql(query, con=con)
-        con.close()
-
+        with pytds.connect(dsn=host,
+                           user=user,
+                           password=password,
+                           database=database) as con:
+            df = pd.read_sql(query, con=con)
+            
         col_map = {}
         for col in df.columns:
             col_map[col] = col
--- tests/integration_tests/data_sources/test_ms_sql_ds.py
+++ tests/integration_tests/data_sources/test_ms_sql_ds.py
@@ -1,8 +1,8 @@
 import pytest
-import pyodbc
+import pytds
 import logging
 from mindsdb_native import Predictor
-from mindsdb_native.libs.data_sources.ms_sql_ds import MSSQLDS, mssql_connect
+from mindsdb_native.libs.data_sources.ms_sql_ds import MSSQLDS
 from mindsdb_native import F
 
 
@@ -14,17 +14,16 @@ def test_mssql_ds():
     DATABASE = 'master'
     PORT = 1433
 
-    con = mssql_connect(host=HOST, port=PORT, user=USER,
-                        password=PASSWORD, database=DATABASE)
-
-    cur = con.cursor()
-
-    cur.execute("IF OBJECT_ID('dbo.test_mindsdb') IS NOT NULL DROP TABLE dbo.test_mindsdb")
-    cur.execute('CREATE TABLE test_mindsdb(col_1 Text, col_2 BIGINT, col_3 BIT)')
-    for i in range(0, 200):
-        cur.execute(f"INSERT INTO test_mindsdb ([col_1], [col_2], [col_3]) VALUES ('This is string number {i}', {i}, {i % 2})")
-    con.commit()
-    con.close()
+    with pytds.connect(dsn=HOST,
+                       user=USER,
+                       password=PASSWORD,
+                       database=DATABASE) as con:
+        with con.cursor() as cur:
+            cur.execute("IF OBJECT_ID('dbo.test_mindsdb') IS NOT NULL DROP TABLE dbo.test_mindsdb")
+            cur.execute('CREATE TABLE test_mindsdb(col_1 Text, col_2 BIGINT, col_3 BIT)')
+            for i in range(0, 200):
+                cur.execute(f"INSERT INTO test_mindsdb ([col_1], [col_2], [col_3]) VALUES ('This is string number {i}', {i}, {i % 2})")
+        con.commit()
 
     mssql_ds = MSSQLDS(table='test_mindsdb', host=HOST, user=USER,
                        password=PASSWORD, database=DATABASE, port=PORT)
