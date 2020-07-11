36720a4bdac763ad9d62add0d11a8323aa368be4
maximlopin <wzzhz13@gmail.com>
2020-07-11 23:58:55 +0300

    refactro datasource

--- mindsdb_native/libs/data_types/data_source.py
+++ mindsdb_native/libs/data_types/data_source.py
@@ -1,9 +1,12 @@
-from mindsdb_native.libs.constants.mindsdb import DATA_TYPES_SUBTYPES, DATA_TYPES, DATA_SUBTYPES
+from mindsdb_native.libs.constants.mindsdb import (
+    DATA_TYPES_SUBTYPES,
+    DATA_TYPES,
+    DATA_SUBTYPES
+)
 from mindsdb_native.libs.data_types.mindsdb_logger import log
 
 
 class DataSource:
-
     def __init__(self, *args, **kwargs):
         self.data_types = {}
         self.data_subtypes = {}
@@ -12,6 +15,9 @@ class DataSource:
         self._cleanup()
 
     def _setup(self, df, **kwargs):
+        """
+        Can be overriden by subclasses
+        """
         col_map = {}
 
         for col in df.columns:
@@ -20,6 +26,9 @@ class DataSource:
         return df, col_map
 
     def _cleanup(self):
+        """
+        Can be overriden by subclasses
+        """
         pass
 
     @property
@@ -27,67 +36,65 @@ class DataSource:
         return self._df
 
     def set_subtypes(self, data_subtypes):
-        if data_subtypes is not None:
-            for col in data_subtypes:
-                if col not in self._col_map:
-                    del data_subtypes[col]
-                    log.warning(f'Column {col} not present in your data, ignoring the "{data_subtypes[col]}" subtype you specified for it')
-
-            self.data_subtypes = data_subtypes
-            for col in self.data_subtypes:
-                col_subtype = self.data_subtypes[col]
-                if col_subtype not in [getattr(DATA_SUBTYPES,x) for x in DATA_SUBTYPES.__dict__ if '__' not in x]:
-                    raise Exception(f'Invalid data subtype: {col_subtype}')
-
-                for col_type in DATA_TYPES_SUBTYPES.subtypes:
-                    if col_subtype in DATA_TYPES_SUBTYPES.subtypes[col_type]:
-                        self.data_types[col] = col_type
+        """
+        :param data_subtypes: dict
+        """
+        for col, subtype in data_subtypes.items():
+            if col not in self._col_map:
+                log.warning(f'Column {col} not present in your data, ignoring the "{subtype}" subtype you specified for it')
+                continue
+            
+            for type_, type_subtypes in DATA_TYPES_SUBTYPES.items():
+                if subtype in type_subtypes:
+                    self.data_types[col] = type_
+                    self.data_subtypes[col] = subtype
+                    break
+            else:
+                raise ValueError(f'Invalid data subtype: {subtype}')
 
     def _set_df(self, df, col_map):
         self._df = df
         self._col_map = col_map
 
-    def dropColumns(self, column_list):
+    def drop_columns(self, column_list):
         """
         Drop columns by original names
 
         :param column_list: a list of columns that you want to drop
-        :return: None
         """
+        columns_to_drop = []
 
-        cols = [col if col not in self._col_map else self._col_map[col] for col in column_list]
-        self._df = self._df.drop(columns=cols)
+        for col in column_list:
+            if col not in self._col_map:
+                columns_to_drop.append(col)
+            else:
+                columns_to_drop.append(self._col_map[col])
 
-    def __getstate__(self): return self.__dict__
-    def __setstate__(self, d): self.__dict__.update(d)
+        self._df.drop(columns=columns_to_drop, inplace=True)
 
-    def __getattr__(self, item):
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, d):
+        self.__dict__.update(d)
+
+    def __getattr__(self, attr):
         """
         Map all other functions to the DataFrame
-
-        :param item: the attribute to get
-        :return: the dataframe attribute
         """
-        if item.startswith('__') and item.endswith('__'):
+        if attr.startswith('__') and attr.endswith('__'):
             raise AttributeError
-        return getattr(self._df, item)
-
+        else:
+            return self._df.__getattr__(attr)
 
     def __getitem__(self, key):
         """
         Map all other items to the DataFrame
-
-        :param key: the key to get
-        :return: the dataframe attribute
         """
-        return self._df[key]
-
+        return self._df.__getitem__(key)
 
     def __setitem__(self, key, value):
         """
-        Support item assignment, mapped ot dataframe
-        :param key:
-        :param value:
-        :return:
+        Support item assignment, mapped to DataFrame
         """
-        self._df[key] = value
+        self._df.__setitem__(key, value)
