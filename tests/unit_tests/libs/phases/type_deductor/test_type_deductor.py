a93a7ab9e4c5d1183e10d8b461647f640b2ad96d
Boris <b.tseytlin@lambda-it.ru>
2020-07-20 17:47:23 +0300

    Remove multiple categories from this pr

--- tests/unit_tests/libs/phases/type_deductor/test_type_deductor.py
+++ tests/unit_tests/libs/phases/type_deductor/test_type_deductor.py
@@ -61,7 +61,6 @@ class TestTypeDeductor:
             'categorical_int': [next(categories_cycle) for i in range(n_points)],
             'categorical_binary': [0, 1] * (n_points//2),
             'sequential_array': [f"1,2,3,4,5,{i}" for i in range(n_points)],
-            'multiple_categories_array': [",".join([str(next(categories_cycle)), str(next(categories_cycle))]) for i in range(n_points)],
             'short_text': generate_short_sentences(n_points),
             'rich_text': generate_rich_sentences(n_points)
         }, index=list(range(n_points)))
