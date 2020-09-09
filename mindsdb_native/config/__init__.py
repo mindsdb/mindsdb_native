bc13417fbb78ab5f12417fa3039b213961643f4b
George3d6 <george@cerebralab.com>
2020-09-09 15:32:10 +0300

    CHECK_FOR_UPDATES fix

--- mindsdb_native/config/__init__.py
+++ mindsdb_native/config/__init__.py
@@ -23,6 +23,8 @@ class Config:
     DEFAULT_LOG_LEVEL = if_env_else('DEFAULT_LOG_LEVEL', CONST.DEBUG_LOG_LEVEL)
 
     CHECK_FOR_UPDATES = if_env_else('CHECK_FOR_UPDATES', True)
+    if CHECK_FOR_UPDATES in [0,'0','false','False']:
+        CHECK_FOR_UPDATES = False
 
     # Default options for unning on sagemaker
     SAGEMAKER = if_env_else('SAGEMAKER', False)
