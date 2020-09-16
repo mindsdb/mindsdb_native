a95347673c1f13f40b2dd1201014c85b43136629
maximlopin <wzzhz13@gmail.com>
2020-09-16 17:51:09 +0300

    add unit test

--- mindsdb_native/libs/phases/data_splitter/data_splitter.py
+++ mindsdb_native/libs/phases/data_splitter/data_splitter.py
@@ -7,7 +7,7 @@ from mindsdb_native.libs.data_types.mindsdb_logger import log
 
 
 class DataSplitter(BaseModule):
-    def run(self):
+    def run(self, test_train_ratio=CONFIG.TEST_TRAIN_RATIO):
         group_by = self.transaction.lmd['tss']['group_by']
 
         NO_GROUP = tuple()
@@ -33,22 +33,21 @@ class DataSplitter(BaseModule):
                 test_indexes[NO_GROUP] = data_split_indexes['test_indexes']
                 validation_indexes[NO_GROUP] = data_split_indexes['validation_indexes']
             else:
-                train_a = 0
-                train_b = round(length - length * CONFIG.TEST_TRAIN_RATIO)
-
-                test_a = train_b
-                test_b = round(length - length * CONFIG.TEST_TRAIN_RATIO) + round(length * CONFIG.TEST_TRAIN_RATIO / 2)
-
-                valid_a = test_b
-                valid_b = length
-
                 if len(group_by) > 0:
                     for group in all_indexes:
-                        length = len(all_indexes[group])
                         if group == NO_GROUP: continue
+                        length = len(all_indexes[group])
 
+                        train_a = 0
+                        train_b = round(length - length * test_train_ratio)
                         train_indexes[group] = all_indexes[group][train_a:train_b]
+
+                        test_a = train_b
+                        test_b = train_b + round(length * test_train_ratio / 2)
                         test_indexes[group] = all_indexes[group][test_a:test_b]
+
+                        valid_a = test_b
+                        valid_b = length
                         validation_indexes[group] = all_indexes[group][valid_a:valid_b]
 
                         train_indexes[NO_GROUP].extend(train_indexes[group])
@@ -56,22 +55,19 @@ class DataSplitter(BaseModule):
                         validation_indexes[NO_GROUP].extend(validation_indexes[group])
                 else:
                     # make sure that the last in the time series are also the subset used for test
-                    length = len(all_indexes[NO_GROUP])
-
                     train_indexes[NO_GROUP] = all_indexes[NO_GROUP][train_a:train_b]
                     test_indexes[NO_GROUP] = all_indexes[NO_GROUP][test_a:test_b]
                     validation_indexes[NO_GROUP] = all_indexes[NO_GROUP][valid_a:valid_b]
 
-
-            self.transaction.input_data.train_df = self.transaction.input_data.data_frame.loc[train_indexes[NO_GROUP]].copy()
-            self.transaction.input_data.test_df = self.transaction.input_data.data_frame.loc[test_indexes[NO_GROUP]].copy()
-            self.transaction.input_data.validation_df = self.transaction.input_data.data_frame.loc[validation_indexes[NO_GROUP]].copy()
+            self.transaction.input_data.train_df = self.transaction.input_data.data_frame.iloc[train_indexes[NO_GROUP]].copy()
+            self.transaction.input_data.test_df = self.transaction.input_data.data_frame.iloc[test_indexes[NO_GROUP]].copy()
+            self.transaction.input_data.validation_df = self.transaction.input_data.data_frame.iloc[validation_indexes[NO_GROUP]].copy()
 
             self.transaction.input_data.data_frame = None
 
-            self.transaction.lmd['data_preparation']['test_row_count'] = len(self.transaction.input_data.test_df)
-            self.transaction.lmd['data_preparation']['train_row_count'] = len(self.transaction.input_data.train_df)
-            self.transaction.lmd['data_preparation']['validation_row_count'] = len(self.transaction.input_data.validation_df)
+            # self.transaction.lmd['data_preparation']['test_row_count'] = len(self.transaction.input_data.test_df)
+            # self.transaction.lmd['data_preparation']['train_row_count'] = len(self.transaction.input_data.train_df)
+            # self.transaction.lmd['data_preparation']['validation_row_count'] = len(self.transaction.input_data.validation_df)
 
             data = {
                 'subsets': [
@@ -84,3 +80,5 @@ class DataSplitter(BaseModule):
 
             self.log.info('We have split the input data into:')
             self.log.infoChart(data, type='pie')
+
+        return all_indexes, train_indexes, test_indexes, validation_indexes
