00a026f4979bd91eac69a899df75b13a1c8a2848
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-10-08 21:23:23 -0300

    revert model_confidence procedure, tests should pass

--- mindsdb_native/libs/backends/lightwood.py
+++ mindsdb_native/libs/backends/lightwood.py
@@ -441,24 +441,28 @@ class LightwoodBackend():
                     for i in range(len(formated_predictions[k])):
                         formated_predictions[k][i].append(predictions[f'{k}_timestep_{timestep_index}']['predictions'][i])
 
-            model_confidence = []
+            model_confidence_dict = {}
             for confidence_name in ['selfaware_confidences','loss_confidences', 'quantile_confidences']:
-
                 if confidence_name in predictions[k]:
+                    if k not in model_confidence_dict:
+                        model_confidence_dict[k] = []
 
                     for i in range(len(predictions[k][confidence_name])):
-                        if len(model_confidence) <= i:
-                            model_confidence.append([])
+                        if len(model_confidence_dict[k]) <= i:
+                            model_confidence_dict[k].append([])
                         conf = predictions[k][confidence_name][i]
                         # @TODO We should make sure lightwood never returns confidences above or bellow 0 and 1
                         if conf < 0:
                             conf = 0
                         if conf > 1:
                             conf = 1
-                        model_confidence[i].append(conf)
+                        model_confidence_dict[k][i].append(conf)
 
-            # @TODO: This is weird
-            formated_predictions[f'{k}_model_confidence'] = [np.mean(x) for x in model_confidence]
+            for k in model_confidence_dict:
+                model_confidence_dict[k] = [np.mean(x) for x in model_confidence_dict[k]]
+
+            for k in model_confidence_dict:
+                formated_predictions[f'{k}_model_confidence'] = model_confidence_dict[k]
 
             if 'confidence_range' in predictions[k]:
                 formated_predictions[f'{k}_confidence_range'] = predictions[k]['confidence_range']
