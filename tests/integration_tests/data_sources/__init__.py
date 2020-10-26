27491d285f09b3d4cf02ad520104c9e3f2e9c33f
maximlopin <wzzhz13@gmail.com>
2020-10-26 14:23:34 +0300

    fix

--- tests/integration_tests/data_sources/__init__.py
+++ tests/integration_tests/data_sources/__init__.py
@@ -2,8 +2,8 @@ import os
 import json
 
 _var_name = 'DATABASE_CREDENTIALS_STRINGIFIED_JSON'
-_var_value = os.getenv(var_name)
+_var_value = os.getenv(_var_name)
 
-assert var_value is not None, var_name + ' ' + 'is not set'
+assert _var_value is not None, _var_name + ' ' + 'is not set'
 
-DB_CREDENTIALS = json.loads(var_value)
+DB_CREDENTIALS = json.loads(_var_value)
