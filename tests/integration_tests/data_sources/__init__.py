2b10de629084125d520de5cff2d37c4428fb0969
maximlopin <wzzhz13@gmail.com>
2020-10-28 11:55:46 +0300

    random run_id for now

--- tests/integration_tests/data_sources/__init__.py
+++ tests/integration_tests/data_sources/__init__.py
@@ -1,6 +1,8 @@
 import os
 import json
-import requests
+import time
+import random
+import string
 
 _var_name = 'DATABASE_CREDENTIALS_STRINGIFIED_JSON'
 _var_value = os.getenv(_var_name)
@@ -9,4 +11,10 @@ assert _var_value is not None, _var_name + ' ' + 'is not set'
 
 DB_CREDENTIALS = json.loads(_var_value)
 
-RUN_ID = os.getenv('GITHUB_RUN_ID')
+# RUN_ID = os.getenv('GITHUB_RUN_ID')
+RUN_ID = ''.join(
+    random.Random(int(time.time() * 1e6)).choices(
+        string.ascii_letters + string.digits,
+        k=16
+    )
+)
