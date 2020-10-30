302e4cc46006a1397baa9e6b0162948c8e73614d
George3d6 <george@cerebralab.com>
2020-10-30 21:43:00 +0200

    polish on the test

--- tests/integration_tests/data_sources/test_snowflake_ds.py
+++ tests/integration_tests/data_sources/test_snowflake_ds.py
@@ -7,6 +7,7 @@ class TestSnowflake(unittest.TestCase):
     def test_snowflake_ds(self):
         from mindsdb_native.libs.data_sources.snowflake_ds import SnowflakeDS
 
+        # Create the datasource
         snowflake_ds = mindsdb_native.SnowflakeDS(
             query='SELECT * FROM HEALTHCARE_COSTS',
             host=DB_CREDENTIALS['snowflake']['host'],
@@ -20,7 +21,8 @@ class TestSnowflake(unittest.TestCase):
             port=DB_CREDENTIALS['snowflake']['port'],
         )
 
-        predictor = mindsdb_native.Predictor(name='healthcare_cost_predictor')
-        predictor.learn(from_data=snowflake_ds, to_predict='CHARGES', stop_training_in_x_seconds=5)
-        example_prediction = predictor.predict(when_data={'AGE':24})
-        assert 'CHARGES' in example_prediction
+        # Make sure we can use it for some basic tasks
+        data_analysis = mindsdb_native.F.analyse_dataset(snowflake_ds, sample_settings={
+            'sample_percentage': 5
+        })
+        assert len(data_analysis['data_analysis_v2']['columns']) == 7
