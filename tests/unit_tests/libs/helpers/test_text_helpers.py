4768376ba5f713af3e3b7e976592454b5f85c654
maximlopin <wzzhz13@gmail.com>
2020-11-05 15:41:15 +0300

    fix

--- tests/unit_tests/libs/helpers/test_text_helpers.py
+++ tests/unit_tests/libs/helpers/test_text_helpers.py
@@ -14,38 +14,39 @@ from mindsdb_native.libs.helpers.text_helpers import get_identifier_description
 
 class TestTextHelpers(unittest.TestCase):
     def test_language_analysis(self):
-        WORDS = {
-            'en': ['because', 'tree', 'merge', 'work', 'interpret', 'call', 'think'],
-            'ru': ['только', 'говорить', 'когда', 'человек', 'быть', 'первый', 'осень'],
-            'de': ['führen', 'stelle', 'heißen', 'konnten', 'schlimm', 'mögen', 'nähe'],
+        SENTENCES = {
+            'en': [
+                'Electromagnetism is a branch of physics involving the study of the electromagnetic force, a type of physical interaction that occurs between electrically charged particles.',
+                'Electromagnetic phenomena are defined in terms of the electromagnetic force, sometimes called the Lorentz force, which includes both electricity and magnetism as different manifestations of the same phenomenon.',
+                'There are numerous mathematical descriptions of the electromagnetic field. In classical electrodynamics, electric fields are described as electric potential and electric current.',
+                'The theoretical implications of electromagnetism, particularly the establishment of the speed of light based on properties of the "medium" of propagation (permeability and permittivity), led to the development of special relativity by Albert Einstein in 1905.',
+            ],
+            'ru': [
+                'Электромагнитное взаимодействие — одно из четырёх фундаментальных взаимодействий. Электромагнитное взаимодействие существует между частицами, обладающими электрическим зарядом.',
+                'С точки зрения квантовой теории поля электромагнитное взаимодействие переносится безмассовым бозоном — фотоном (частицей, которую можно представить как квантовое возбуждение электромагнитного поля).',
+                'Электромагнитное взаимодействие отличается от слабого и сильного взаимодействия своим дальнодействующим характером — сила взаимодействия между двумя зарядами спадает только как вторая степень расстояния.',
+                'В классических (неквантовых) рамках электромагнитное взаимодействие описывается классической электродинамикой.',
+            ],
+            'de': [
+                'Die elektromagnetische Wechselwirkung ist eine der vier Grundkräfte der Physik. Wie die Gravitation ist sie im Alltag leicht erfahrbar, daher ist sie seit langem eingehend erforscht und seit über 100 Jahren gut verstanden.',
+                'Ausgangspunkt der Erforschung war eine Untersuchung der Kräfte zwischen elektrischen Ladungen. Das Gesetz von Coulomb von etwa 1785 gibt diese Kraftwirkung zwischen zwei punktförmigen Ladungen ganz analog zum Gravitationsgesetz an.',
+                'Die Theorie der klassischen Elektrodynamik geht auf James Clerk Maxwell zurück, der im 19. Jahrhundert in den nach ihm benannten Maxwell-Gleichungen die Gesetze der Elektrizität, des Magnetismus und des Lichts als verschiedene Aspekte einer grundlegenden Wechselwirkung, des Elektromagnetismus, erkannte.',
+                'Im Bereich der kleinsten Teilchen wird die elektromagnetische Wechselwirkung durch die Quantenelektrodynamik beschrieben. Die elektromagnetischen Potentiale werden darin als Feldoperatoren aufgefasst, durch diese werden die Photonen, die Wechselwirkungsteilchen der elektromagnetischen Wechselwirkung, erzeugt oder vernichtet.',
+            ],
         }
 
-        sent_size = 7
-        num_sents = 10
+        for lang, sentences in SENTENCES.items():
+            nr_words, word_dist, nr_words_dist = analyze_sentences(sentences)
+            lang_dist = get_language_dist(sentences)
 
-        for lang, words in WORDS.items():
-            sentences = [random.sample(words, sent_size) for _ in range(num_sents)]
+            assert nr_words > 10
+            assert len(word_dist) > 10
+            assert len(nr_words_dist) > 0
 
-            nr_words, word_dist, nr_words_dist = analyze_sentences(' '.join(sent) for sent in sentences)
+            assert 'Unknown' in lang_dist
+            assert lang_dist['Unknown'] == 0
+            assert lang_dist[lang] == len(SENTENCES[lang])
 
-            assert nr_words == len(sentences * sent_size)
-
-            expected_dist_count = 0
-
-            total_count = 100
-            for _ in range(total_count):
-                lang_dist = get_language_dist(' '.join(sent) for sent in sentences)
-                
-                assert 'Unknown' in lang_dist
-
-                if lang_dist[lang] == len(sentences):
-                    if lang_dist['Unknown'] == 0:
-                        expected_dist_count += 1
-
-            # langdetect is not determenistic, and because text size
-            # and vocabulary are small in this test, we expect that
-            # most of the time (70%) it detects languages correctly
-            assert (expected_dist_count / total_count) > 0.70
 
     def test_identifiers(self):
         N = 300
