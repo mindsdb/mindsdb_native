1910b8dc21fd2af5ea968d085d2b94ca504a8963
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-11-10 12:23:13 -0300

    fixed bug in use_mixers advanced arg

--- mindsdb_native/libs/controllers/predictor.py
+++ mindsdb_native/libs/controllers/predictor.py
@@ -260,7 +260,7 @@ class Predictor:
                 data_split_indexes = advanced_args.get('data_split_indexes', None),
                 tags_delimiter = advanced_args.get('tags_delimiter', ','),
                 force_predict = advanced_args.get('force_predict', False),
-                mixer_class = advanced_args.get('use_mixers', None),
+                use_mixers = advanced_args.get('use_mixers', None),
                 setup_args = from_data.setup_args if hasattr(from_data, 'setup_args') else None,
                 debug = advanced_args.get('debug', False),
                 allow_incomplete_history = advanced_args.get('allow_incomplete_history', False),
