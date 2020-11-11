a30c1b6055b976a30bcfde65207e091d8c2bac17
George3d6 <george@cerebralab.com>
2020-11-11 23:30:29 +0200

    added filtering both for pandas and for sql

--- mindsdb_native/libs/data_types/data_source.py
+++ mindsdb_native/libs/data_types/data_source.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import moz_sql_parser
+from moz_sql_parser.keywords import binary_ops
+
 from mindsdb_native.libs.constants.mindsdb import (
     DATA_TYPES_SUBTYPES,
     DATA_TYPES,
@@ -10,7 +14,10 @@ class DataSource:
     def __init__(self, *args, **kwargs):
         self.data_types = {}
         self.data_subtypes = {}
+        self.is_dynamic = False
         df, col_map = self._setup(*args, **kwargs)
+        self.args = args
+        self.kwargs = kwargs
         self._set_df(df, col_map)
         self._cleanup()
 
@@ -39,7 +46,7 @@ class DataSource:
             if col not in self._col_map:
                 log.warning(f'Column {col} not present in your data, ignoring the "{subtype}" subtype you specified for it')
                 continue
-            
+
             for type_, type_subtypes in DATA_TYPES_SUBTYPES.items():
                 if subtype in type_subtypes:
                     self.data_types[col] = type_
@@ -67,6 +74,55 @@ class DataSource:
                 columns_to_drop.append(self._col_map[col])
 
         self.df.drop(columns=columns_to_drop, inplace=True)
+
+    def _filter_to_pandas(self, raw_condition):
+        """Convert filter conditions to a paticular
+        DataFrame instance"""
+        mapping = {
+                    ">": lambda x, y: self._df[x] > y,
+                    "LIKE": lambda x, y: self._df[x].str.contains(y.replace("%", "")),
+                    "<": lambda x, y: self._df[x] < y,
+                    "=": lambda x, y: self._df[x] == y,
+                    "!=": lambda x, y: self._df[x] != y
+                  }
+        col, cond, val = raw_condition
+        return mapping[cond](col, val)
+
+    def filter(self, where=None, limit=None):
+        """Convert SQL like filter requests to pandas DataFrame filtering"""
+        if self.is_dynamic:
+            parsed_query = moz_sql_parser.parse(self.query)
+
+            for col, op, value in where or []:
+                past_where_clause = self.parsed.get('where', {})
+
+                op = op.lower()
+                op_json = binary_ops.get(op, None)
+                if op_json is None:
+                    log.warning(f"Operator: {op} not found in: QueryBuilder._OPERATORS\n Using it anyway.")
+                    op_json = op.lower()
+
+                if op.lower() == 'like':
+                    value = '%' + value.strip('%') + '%'
+
+                where_clause = {op_json: [col, value]}
+
+                if len(past_where_clause) > 0:
+                    where_clause = {'and': [where_clause, past_where_clause]}
+
+                parsed_query['where'] = where_clause
+
+            if limit is not None:
+                parsed_query['limit'] = limit
+
+            query = moz_sql_parser.format(parsed_query)
+            
+            return self._setup(*self.args, query=query, **self.kwargs)._df
+        else:
+            if where:
+                for cond in [self._filter_to_pandas(x) for x in where]:
+                    self._df = self._df[cond]
+            return self._df.head(limit) if limit else self._df
 
     def __getstate__(self):
         return self.__dict__
