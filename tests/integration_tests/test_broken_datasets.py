4a5d7ccaa74f3b0d9bf9cca3ac7933b5edae0413
maximlopin <wzzhz13@gmail.com>
2020-11-16 17:29:29 +0300

    test home rentals and hdi

---
+++ tests/integration_tests/test_broken_datasets.py
@@ -0,0 +1,38 @@
+from .data_sources import ClickhouseTest, break_dataset
+from mindsdb_native import F
+
+
+class TestBrokenDatasets(ClickhouseTest):
+    def get_ds(self, table, limit=300):
+        from mindsdb_native import ClickhouseDS
+        return ClickhouseDS(
+            host=self.HOST,
+            port=self.PORT,
+            user=self.USER,
+            password=self.PASSWORD,
+            query='SELECT * FROM {}.{} LIMIT {}'.format(
+                self.DATABASE,
+                table,
+                limit
+            )
+        )
+
+    def test_home_rentals(self):
+        ds = self.get_ds('home_rentals', limit=500)
+        stats_1 = F.analyse_dataset(ds)['data_analysis_v2']
+
+        ds.df = break_dataset(ds.df)
+        stats_2 = F.analyse_dataset(ds)['data_analysis_v2']
+
+        for col in ds.df.columns:
+            assert stats_1[col]['typing']['data_type'] == stats_2[col]['typing']['data_type']
+
+    def test_hdi(self):
+        ds = self.get_ds('hdi', limit=500)
+        stats_1 = F.analyse_dataset(ds)['data_analysis_v2']
+
+        ds.df = break_dataset(ds.df)
+        stats_2 = F.analyse_dataset(ds)['data_analysis_v2']
+
+        for col in ds.df.columns:
+            assert stats_1[col]['typing']['data_type'] == stats_2[col]['typing']['data_type']
