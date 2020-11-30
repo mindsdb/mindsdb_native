bed85b2e6f1297d88cb49d82d81c1045d03977ff
George3d6 <george@cerebralab.com>
2020-11-30 14:56:17 +0200

    fixed string casting

--- mindsdb_native/libs/data_types/data_source.py
+++ mindsdb_native/libs/data_types/data_source.py
@@ -97,7 +97,6 @@ class DataSource:
         self._internal_df.drop(columns=columns_to_drop, inplace=True)
 
     def query(self, q=None):
-        print('\n\n\n', q, '\n\n\n')
         """
         :param q: a query specific to type of datasource
         Datasources must override this method to return pandas.DataFrame
@@ -181,20 +180,27 @@ class SQLDataSource(DataSource):
 
     def filter(self, where=None, limit=None, get_col_map=False):
         try:
-            print(self._query)
-            parsed_query = moz_sql_parser.parse(self._query.replace('FORMT JSON'))
+            parsed_query = moz_sql_parser.parse(self._query.replace('FORMAT JSON', ''))
 
             for col, op, value in where or []:
                 past_where_clause = parsed_query.get('where', {})
 
                 op = op.lower()
                 op_json = binary_ops.get(op, None)
+
                 if op_json is None:
-                    log.warning(f"Operator: {op} not found in: QueryBuilder._OPERATORS\n Using it anyway.")
+                    log.warning(f"Operator: {op} not found in the sql parser operator list\n Using it anyway.")
                     op_json = op
 
                 if op == 'like':
                     value = '%' + value.strip('%') + '%'
+                    if 'clickhouse' in self.name().lower():
+                        col = f'toString({col})'
+                    elif 'postgres' in self.name().lower():
+                        col = f'{col}::text'
+                    elif 'mariadb' in self.name().lower() or 'mysql' in self.name().lower() or 'mssql' in self.name().lower():
+                        col = f'CAST({col} AS TEXT)'
+
 
                 where_clause = {op_json: [col, value]}
 
@@ -209,6 +215,8 @@ class SQLDataSource(DataSource):
             query = moz_sql_parser.format(parsed_query)
             query = query.replace('"', "'")
 
+
+            print('\n\n\n\n', query, '\n\n\n\n')
             if get_col_map:
                 return self.query(query)
             else:
