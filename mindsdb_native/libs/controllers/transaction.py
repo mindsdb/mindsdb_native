dd3ed5492aa94f69e62104ade63cc1a49e21519c
maximlopin <wzzhz13@gmail.com>
2020-12-02 22:12:41 +0300

    fix

--- mindsdb_native/libs/controllers/transaction.py
+++ mindsdb_native/libs/controllers/transaction.py
@@ -235,7 +235,7 @@ class LearnTransaction(Transaction):
             if self.lmd['quick_learn'] is None:
                 n_cols = len(self.input_data.columns)
                 n_cells = n_cols * len(self.input_data.sample_df())
-                if n_cols >= 80 and n_cells > int(1e5) and not self.lmd['force_model_analysis']:
+                if n_cols >= 80 and n_cells > int(1e5):
                     self.log.warning('Data has too many columns, setting quick_learn to True')
                     self.lmd['quick_learn'] = True
 
