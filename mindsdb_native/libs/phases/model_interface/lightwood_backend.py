c3b70b98c1de5f09c78e1e1ef643dea73058420c
maximlopin <wzzhz13@gmail.com>
2020-12-04 14:43:04 +0300

    fix

--- mindsdb_native/libs/phases/model_interface/lightwood_backend.py
+++ mindsdb_native/libs/phases/model_interface/lightwood_backend.py
@@ -409,8 +409,7 @@ class LightwoodBackend:
                 self.transaction.input_data.validation_df[self.transaction.input_data.validation_df['make_predictions'].astype(bool) == True] if self.transaction.lmd['tss']['is_timeseries'] else self.transaction.input_data.validation_df,
                 self.transaction.lmd['stats_v2'],
                 self.transaction.lmd['predict_columns'],
-                backend=self,
-                use_conf_intervals=False # r2_score will be used for regression
+                backend=self
             )
 
             predictors_and_accuracies.append((
