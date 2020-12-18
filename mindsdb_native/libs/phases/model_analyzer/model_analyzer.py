f555035dc92281273ec12a0c5fa8ba460f8a58ad
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-12-18 19:17:41 -0300

    omit historical_columns for ignorable_input analysis

--- mindsdb_native/libs/phases/model_analyzer/model_analyzer.py
+++ mindsdb_native/libs/phases/model_analyzer/model_analyzer.py
@@ -86,7 +86,9 @@ class ModelAnalyzer(BaseModule):
         empty_input_predictions_test = {}
 
         ignorable_input_columns = [x for x in input_columns if self.transaction.lmd['stats_v2'][x]['typing']['data_type'] != DATA_TYPES.FILE_PATH
-                           and (not self.transaction.lmd['tss']['is_timeseries'] or x not in self.transaction.lmd['tss']['order_by'])]
+                                   and (not self.transaction.lmd['tss']['is_timeseries'] or
+                                        (x not in self.transaction.lmd['tss']['order_by'] and
+                                         x not in self.transaction.lmd['tss']['historical_columns']))]
 
         for col in ignorable_input_columns:
             empty_input_predictions[col] = self.transaction.model_backend.predict('validate', ignore_columns=[col])
