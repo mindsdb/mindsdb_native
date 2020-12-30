1240eec5652c48d3ea30ed17cee24a86472893d0
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-12-30 19:26:23 -0300

    rm print

--- mindsdb_native/libs/controllers/functional.py
+++ mindsdb_native/libs/controllers/functional.py
@@ -241,7 +241,6 @@ def get_model_data(model_name=None, lmd=None):
         pass
     elif model_name is not None:
         with MDBLock('shared', 'get_data_' + model_name):
-            print(CONFIG.MINDSDB_STORAGE_PATH, model_name, 'light_model_metadata.pickle')
             lmd = load_lmd(os.path.join(CONFIG.MINDSDB_STORAGE_PATH, model_name, 'light_model_metadata.pickle'))
 
     # ADAPTOR CODE
