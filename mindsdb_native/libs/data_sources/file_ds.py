941701ee6be3ffc2f2f4615a5501e542805fa552
George3d6 <george@cerebralab.com>
2021-02-04 23:46:59 +0000

    addressed more naming conflicts

--- mindsdb_native/libs/data_sources/file_ds.py
+++ mindsdb_native/libs/data_sources/file_ds.py
@@ -38,7 +38,6 @@ class FileDS(DataSource):
         self.clean_rows = clean_rows
         self.custom_parser = custom_parser
         self.dialect = None
-        self.col_map = None
 
     def _handle_source(self):
         self._file_name = os.path.basename(self.file)
@@ -62,10 +61,13 @@ class FileDS(DataSource):
             file_data = df.values.tolist()
 
         elif fmt == 'json':
+            print(1)
             data.seek(0)
             json_doc = json.loads(data.read())
+            print(2)
             df = pd.json_normalize(json_doc, max_level=0)
             header = df.columns.values.tolist()
+            print(3)
             file_data = df.values.tolist()
 
         else:
@@ -76,8 +78,8 @@ class FileDS(DataSource):
         else:
             file_list_data = file_data
 
-        self.col_map = dict((col, col) for col in header)
-        return pd.DataFrame(file_list_data, columns=header), self.col_map
+        col_map = dict((col, col) for col in header)
+        return pd.DataFrame(file_list_data, columns=header), col_map
 
     def query(self, q=None):
         try:
@@ -85,7 +87,9 @@ class FileDS(DataSource):
         except Exception as e:
             log.error(f"Error creating dataframe from handled data: {e}")
             log.error("pd.read_csv data handler would be used.")
-            return pd.read_csv(self.file, sep=self.dialect.delimiter), self.col_map
+            df = pd.read_csv(self.file, sep=self.dialect.delimiter)
+            col_map = dict((col, col) for col in df.columns)
+            return df, col_map
 
     def _getDataIo(self, file):
         """
