f5140db0d36db430cbd410c6bc4ea4479c312d4e
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-02-23 17:17:50 -0300

    fix

--- mindsdb_native/libs/helpers/mp_helpers.py
+++ mindsdb_native/libs/helpers/mp_helpers.py
@@ -10,7 +10,7 @@ def get_nr_procs(max_processes=None, max_per_proc_usage=None, df=None):
         available_mem = psutil.virtual_memory().available
         if max_per_proc_usage is None or type(max_per_proc_usage) not in (int, float):
             max_per_proc_usage = 2.6 * pow(10, 9)
-            if df:
+            if df is not None:
                 max_per_proc_usage += df.memory_usage(index=True, deep=True).sum()
         proc_count = int(min(mp.cpu_count(), available_mem // max_per_proc_usage)) - 1
         if isinstance(max_processes, int):
