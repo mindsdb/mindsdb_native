8a4029cd8dc928f90bd3be0ee7395619c6508e5b
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-04-02 20:17:57 -0300

    stff

--- mindsdb_native/libs/controllers/transaction.py
+++ mindsdb_native/libs/controllers/transaction.py
@@ -432,10 +432,13 @@ class PredictTransaction(Transaction):
                     # convert (B, 2, 99) into (B, 2) given width or error rate constraints
                     if is_numerical:
                         error_rate = self.lmd['anomaly_error_rate'] if is_anomaly_task else None
+                        # significances = 0.8
+                        # confs = all_confs[:, :, int(100*(1-significances))-1]
                         significances, confs = get_numerical_conf_range(all_confs,
                                                                         predicted_col,
                                                                         self.lmd['stats_v2'],
                                                                         error_rate=error_rate)
+                        print(f"SIGNIFICANCE AT {significances}")
                         result.loc[X.index, 'lower'] = confs[:, 0]
                         result.loc[X.index, 'upper'] = confs[:, 1]
                     else:
