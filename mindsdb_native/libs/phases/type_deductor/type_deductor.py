b5ab753b480eb38b6cb80e38dd5d1b3edabc0844
maximlopin <wzzhz13@gmail.com>
2021-04-08 18:50:57 +0300

    a

--- mindsdb_native/libs/phases/type_deductor/type_deductor.py
+++ mindsdb_native/libs/phases/type_deductor/type_deductor.py
@@ -183,7 +183,7 @@ def get_column_data_type(arg_tup, lmd):
     type_dist, subtype_dist, new_additional_info = count_data_types_in_column(
         data,
         date_fmts=lmd.get('date_fmts') or ['%Y-%m-%d', '%Y/%m/%d', '%d.%m.%Y', '%Y/%m'],
-        datetime_fmts=lmd.get('datetime_fmts') or ['%Y-%m-%d %H:%M:%S', '%Y/%m/%d %H:%M:%S', '%d.%m.%Y %H:%M:%S', '%Y-%m-%dT%H:%M:%S.%f%Z', '%Y-%m-%dT%H:%M:%S.%f%']
+        datetime_fmts=lmd.get('datetime_fmts') or ['%Y-%m-%d %H:%M:%S', '%Y/%m/%d %H:%M:%S', '%d.%m.%Y %H:%M:%S', '%Y-%m-%dT%H:%M:%S.%f%Z', '%Y-%m-%dT%H:%M:%S.%f%z', '%Y-%m-%dT%H:%M:%S.%f%']
     )
 
     if new_additional_info:
