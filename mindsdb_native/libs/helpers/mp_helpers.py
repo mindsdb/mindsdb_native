91ad1df23e45bc59dd8a1c52b012abbfc5b24bec
George3d6 <george@cerebralab.com>
2021-05-22 00:05:41 +0100

    reverted

--- mindsdb_native/libs/helpers/mp_helpers.py
+++ mindsdb_native/libs/helpers/mp_helpers.py
@@ -13,7 +13,7 @@ def get_nr_procs(max_processes=None, max_per_proc_usage=None, df=None):
                 import mindsdb_worker
                 max_per_proc_usage = 0.2 * pow(10,9)
             except:
-                max_per_proc_usage = 4 * pow(10, 9)
+                max_per_proc_usage = 2.6 * pow(10, 9)
 
             if df is not None:
                 max_per_proc_usage += df.memory_usage(index=True, deep=True).sum()
