29c134d28670fe874fcaa4304844c8e24366893a
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-06-25 12:34:22 -0400

    add categorical test

--- tests/integration_tests/flows/test_finetune.py
+++ tests/integration_tests/flows/test_finetune.py
@@ -1,31 +1,30 @@
 import unittest
 import mindsdb_native
 from mindsdb_native import Predictor
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, balanced_accuracy_score
+from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
+np.random.seed(42)
 
 
 class TestFinetune(unittest.TestCase):
-    def test_finetune(self):
-        df = pd.read_csv("https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv")
-        target = 'rental_price'
-        grouped_col = 'neighborhood'
+    def finetune_helper(self, df, target, grouped_col, acc_func, pred_name):
         test_size=0.2
         groups = df[grouped_col].unique()
         data_tuples = []
+
         for group in groups:
             grouped = df[df[grouped_col] == group]
             train = grouped[:int(len(grouped)*(1-test_size))]
             test = grouped[int(len(grouped)*(1-test_size)):]
             if len(data_tuples) > 0:
-                all_test = test.append(data_tuples[-1][1])  # add previous groups to the latest test DF
+                all_test = test.append(data_tuples[-1][1])  # adds previous groups to the latest test DF
             else:
                 all_test = test
             data_tuples.append((train, test, all_test))
 
-        predictor = Predictor(name='home_rentals_finetune')
-
+        predictor = Predictor(name=pred_name)
         results_single_test = []
         results_all_test = []
         for i, (train, test, all_test) in enumerate(data_tuples):
@@ -33,17 +32,38 @@ class TestFinetune(unittest.TestCase):
                 predictor.learn(from_data=train,
                                 to_predict=target,
                                 stop_training_in_x_seconds=10,
-                                advanced_args={'use_mixers': ['NnMixer']},
+                                advanced_args={'use_mixers': ['LightGBMMixer']},
                                 use_gpu=False)
             else:
                 # finetune here
-                predictor.adjust(from_data=train)  #  TODO: maybe add interface here for additional options
+                predictor.adjust(from_data=train)
 
-            result_single = predictor.test(when_data=test, accuracy_score_functions=r2_score)
-            result_all = predictor.test(when_data=all_test, accuracy_score_functions=r2_score)
+            result_single = predictor.test(when_data=test, accuracy_score_functions=acc_func)
+            result_all = predictor.test(when_data=all_test, accuracy_score_functions=acc_func)
 
             results_single_test.append(result_single[f'{target}_accuracy'])
             results_all_test.append(result_all[f'{target}_accuracy'])
 
-        assert np.array(results_single_test).mean() >= 0.9
-        assert np.array(results_all_test).mean() >= 0.9
+        return results_single_test, results_all_test
+
+    def test_finetune_numerical(self):
+        df = pd.read_csv("https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv")
+        accs_single_split, accs_all_data = self.finetune_helper(df,
+                                                                'rental_price',
+                                                                'neighborhood',
+                                                                r2_score,
+                                                                '__test_finetune_numerical')
+        assert np.array(accs_single_split).mean() >= 0.9
+        assert np.array(accs_all_data).mean() >= 0.9
+
+    def test_finetune_classification(self):
+        df = load_iris(as_frame=True).frame
+        df['finetune_group'] = np.random.randint(3, size=df.shape[0])  # random splits to finetune
+
+        accs_single_split, accs_all_data = self.finetune_helper(df,
+                                                                'target',
+                                                                'finetune_group',
+                                                                balanced_accuracy_score,
+                                                                '__test_finetune_categorical')
+        assert np.array(accs_single_split).mean() >= 0.9
+        assert np.array(accs_all_data).mean() >= 0.9
